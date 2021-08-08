2d04cb5f47c271cdcf0a27c311b05f3749ffb22e
Mark Gire <markgire@Marks-MacBook-Pro.local>
2021-08-08 16:06:40 -0400

    added assets folder and fixed poem entry

--- src/components/PoemCard.js
+++ src/components/PoemCard.js
@@ -1,8 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 
-import { Container, Grid, Card, CardActionArea, Typography, Dialog, DialogTitle, DialogContent, DialogContentText, DialogActions, Paper, Button, useMediaQuery, useTheme } from "@material-ui/core";
+import { Grid, Card, CardActionArea, Typography, Dialog, DialogTitle, DialogContent, DialogContentText, DialogActions, Button, useMediaQuery, useTheme } from "@material-ui/core";
 
 const PoemCard = (props) => {
+
+    const [showPoem, setShowPoem] = useState({
+        expand: false,
+        open: false
+
+    })
+
+    const theme = useTheme();
+    const fullScreen = useMediaQuery(theme.breakpoints.down('sm'));
+
+    const handleOpen = () => {
+        setShowPoem({ ...showPoem, open: true });
+      };
+    
+    const handleClose = () => {
+        setShowPoem({ ...showPoem, open: false });
+    };
+
+    const handleExpand = () => {
+        setShowPoem({ ...showPoem, expand: true });
+    }
+
+    const handleCollapse = () => {
+        setShowPoem({ ...showPoem, expand: false });
+    }
     
     const style = {
         card: {
@@ -36,22 +61,40 @@ const PoemCard = (props) => {
     return (
         <>
             <Card style={style.entry}>
-                <CardActionArea>
+                <CardActionArea onClick={handleOpen}>
                     <Typography variant="h4">{props.info.title}</Typography>
-                    <Card style={style.cardLong}>
+                    <Card style={!showPoem.expand ? style.cardLong : style.card}>
                         <Typography>{props.text}</Typography>
                     </Card>
-                    
                 </CardActionArea>
-                
             </Card>
-            <Button style={style.continue}>Continue Reading</Button>
+            {!showPoem.expand ? <Button onClick={handleExpand}style={style.continue}>Continue Reading</Button> : <Button onClick={handleCollapse} style={style.continue}>Collapse</Button>}
+            <Dialog
+                fullScreen={fullScreen}
+                open={showPoem.open}
+                onClose={handleClose}
+                aria-labelledby="responsive-dialog-title"
+                >
+                <DialogTitle id="responsive-dialog-title">{props.info.title}</DialogTitle>
+                <DialogContent>
+                    <DialogContentText>
+                        <Typography>{props.text}</Typography>
+                    </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <Grid container alignItems="right">
+                        <Button onClick={handleClose} color="primary" autoFocus>
+                            Back
+                        </Button>
+                    </Grid>
+                </DialogActions>
+            </Dialog>
         </>
     )
 
     return (
         <>
-            <Card style={style.entry}>
+            <Card style={style.entry} onClick={handleOpen}>
                 <CardActionArea>
                     <Typography variant="h4">{props.info.title}</Typography>
                     <Card style={style.card}>
@@ -59,6 +102,26 @@ const PoemCard = (props) => {
                     </Card>
                 </CardActionArea>
             </Card>
+            <Dialog
+                fullScreen={fullScreen}
+                open={showPoem.open}
+                onClose={handleClose}
+                aria-labelledby="responsive-dialog-title"
+                >
+                <DialogTitle id="responsive-dialog-title">{props.info.title}</DialogTitle>
+                <DialogContent>
+                    <DialogContentText>
+                        <Typography>{props.text}</Typography>
+                    </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <Grid container alignItems="right">
+                        <Button onClick={handleClose} color="primary" autoFocus>
+                            Back
+                        </Button>
+                    </Grid>
+                </DialogActions>
+            </Dialog>
         </>
     )
 }
