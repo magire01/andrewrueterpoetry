8942265cbd6f6687bd6bd4c39195e69919ece4ea
Mark Gire <markgire@Marks-MacBook-Pro.local>
2021-08-08 15:13:18 -0400

    Styled poem entry

--- src/components/PoemCard.js
+++ src/components/PoemCard.js
@@ -1,14 +1,60 @@
 import React from "react";
 
-import { Grid, Card, CardActionArea, Typography, Dialog, DialogTitle, DialogContent, DialogContentText, DialogActions, Button, useMediaQuery, useTheme } from "@material-ui/core";
+import { Container, Grid, Card, CardActionArea, Typography, Dialog, DialogTitle, DialogContent, DialogContentText, DialogActions, Paper, Button, useMediaQuery, useTheme } from "@material-ui/core";
 
 const PoemCard = (props) => {
+    
+    const style = {
+        card: {
+            height: "auto",
+            width: "90%",
+            border: "none",
+            boxShadow: "none"
+        },
+        cardLong: {
+            height: 200,
+            width: "90%",
+            border: "none",
+            boxShadow: "none"
+        },
+        entry: {
+            width: "auto",
+            height: "auto",
+            border: "none",
+            boxShadow: "none",
+            width: "auto",
+            height: "auto"
+        },
+        continue: {
+            color: "red"
+        }
+    }
+
+    const poemLength = props.info.text.length;
+
+    if (poemLength > 150)
     return (
         <>
-            <Card>
+            <Card style={style.entry}>
                 <CardActionArea>
-                    <Card>
-                        <Typography variant="h4">{props.info.title}</Typography>
+                    <Typography variant="h4">{props.info.title}</Typography>
+                    <Card style={style.cardLong}>
+                        <Typography>{props.text}</Typography>
+                    </Card>
+                    
+                </CardActionArea>
+                
+            </Card>
+            <Button style={style.continue}>Continue Reading</Button>
+        </>
+    )
+
+    return (
+        <>
+            <Card style={style.entry}>
+                <CardActionArea>
+                    <Typography variant="h4">{props.info.title}</Typography>
+                    <Card style={style.card}>
                         <Typography>{props.text}</Typography>
                     </Card>
                 </CardActionArea>
--- src/pages/Poems.js
+++ src/pages/Poems.js
@@ -7,17 +7,24 @@ import PoemCard from '../components/PoemCard';
 
 
 const Poems = () => {
+
+    const style= {
+        column: {
+            height: "auto",
+            width: "auto"
+        }
+    }
     return (
 
         <Grid
             container
             direction="row"
             justifyContent="center"
-            alignItems="baseline"
-            spacing={2}
+            alignItems="center"
+            spacing={10}
             >
             {PoemAPI.data.map(film => (
-            <Grid item><PoemCard info={film} text={nl2br(film.text)}/></Grid>
+            <Grid item md={8} xs={12} style={style.column}><PoemCard info={film} text={nl2br(film.text)}/></Grid>
                 ))}
         </Grid>
     )
